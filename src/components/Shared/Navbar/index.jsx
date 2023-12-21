c11b291da8e179b33ad7d07181f6d37b9bbe392d
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-21 18:47:46 +0600

    menu items added

--- src/components/Shared/Navbar/index.jsx
+++ src/components/Shared/Navbar/index.jsx
@@ -1,20 +1,51 @@
 /* eslint-disable react/jsx-no-undef */
 "use client"
 import React, { useState } from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button,NavbarMenuToggle, NavbarMenu, NavbarMenuItem  } from "@nextui-org/react";
-import {Image} from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem,  Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
+import Link from "next/link"
 
 export default function PrimaryNavbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const menuItems = [
-        "Home",
-        "Booking",
-        "About Us",
-        "Our Article",
-        "Contact Us",
-        "Log Out",
-    ];
+    // const menuItems = [
+
+    //     "Home",
+    //     "Booking",
+    //     "About Us",
+    //     "Our Article",
+    //     "Contact Us",
+    //     "Log Out",
+    // ];
+
+    const primaryMenuItems = [
+        {
+            id: 1,
+            title: "Home",
+            link: "/"
+        },
+        {
+            id: 2,
+            title: "Booking",
+            link: "/booking"
+        },
+        {
+            id: 3,
+            title: "About Us",
+            link: "/aboutUs"
+        },
+        {
+            id: 4,
+            title: "Contact Us",
+            link: "/contactUs"
+        },
+        {
+            id: 5,
+            title: "Article",
+            link: "/article"
+        }
+
+    ]
 
     return (
         <Navbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll className="bg-black text-white">
@@ -23,38 +54,50 @@ export default function PrimaryNavbar() {
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                     className="sm:hidden"
                 />
+                 <Link href={'/'}>
                 <NavbarBrand>
-                   <Image src={"/MasterApplianceTechLogo.jpg"} alt="MasterApplianceTechLogo" width={90} height={100}/>
+                   
+                    <Image src={"/MasterApplianceTechLogo.jpg"} alt="MasterApplianceTechLogo" width={90} height={100} />
                     <p className="font-bold text-inherit">Master Appliance</p>
+                   
                 </NavbarBrand>
+                </Link>
             </NavbarContent>
 
+
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem >
+                {
+                    primaryMenuItems?.map((primaryMenuItem) => <NavbarItem key={primaryMenuItem?.id}>
+                        <Link className="text-white" color="foreground" href={primaryMenuItem?.link}>
+                            {primaryMenuItem?.title}
+                        </Link>
+                    </NavbarItem>)
+                }
+                {/* <NavbarItem >
                     <Link className="text-white" color="foreground" href="#">
-                    Home
+                        Home
                     </Link>
                 </NavbarItem>
                 <NavbarItem isActive>
-                    <Link  className="text-white"  href="#" aria-current="page">
-                    Booking
+                    <Link className="text-white" href="#" aria-current="page">
+                        Booking
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link className="text-white"  color="foreground" href="#">
-                    About Us 
+                    <Link className="text-white" color="foreground" href="#">
+                        About Us
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link className="text-white"  color="foreground" href="#">
-                    Our Article
+                    <Link className="text-white" color="foreground" href="#">
+                        Our Article
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link  className="text-white" color="foreground" href="#">
-                    Contact Us
+                    <Link className="text-white" color="foreground" href="#">
+                        Contact Us
                     </Link>
-                </NavbarItem>
+                </NavbarItem> */}
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
@@ -67,17 +110,17 @@ export default function PrimaryNavbar() {
                 </NavbarItem>
             </NavbarContent>
             <NavbarMenu>
-                {menuItems.map((item, index) => (
+                {primaryMenuItems.map((item, index) => (
                     <NavbarMenuItem key={`${item}-${index}`}>
                         <Link
                             color={
-                                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                                index === 2 ? "primary" : index === NavbarMenuItem.length - 1 ? "danger" : "foreground"
                             }
                             className="w-full"
-                            href="#"
+                            href={item?.link}
                             size="lg"
                         >
-                            {item}
+                            {item?.title}
                         </Link>
                     </NavbarMenuItem>
                 ))}
