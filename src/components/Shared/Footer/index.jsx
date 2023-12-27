f5c60589197c410fc3fc30241e00e53b9936a0b6
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-28 01:47:46 +0600

    date added

--- src/components/Shared/Footer/index.jsx
+++ src/components/Shared/Footer/index.jsx
@@ -241,7 +241,7 @@ export default function Footer() {
                 </div>
 
                 <div className="w-full p-4 text-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                    {&copy new Date().getFullYear() } Copyright:
+                    {new Date().getFullYear() &copy } Copyright:
                     <a className="text-white" href="https://akmsyful.vercel.app/">Digitize Solutions</a>
                 </div>
 
