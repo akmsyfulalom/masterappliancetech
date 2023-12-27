02bda5a1320c5b61ccf92f0968d1051b8c230b10
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-28 04:49:04 +0600

    footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ const config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
@@ -17,7 +18,7 @@ const config = {
       },
     },
   },
-  plugins: [nextui()],
+  plugins:[nextui()]
 };
 
 module.exports = config;
