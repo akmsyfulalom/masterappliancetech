781906392973744e86bd39388111528733ef0cee
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-28 06:47:40 +0600

    hero section added

--- src/components/Home/Booking/index.jsx
+++ src/components/Home/Booking/index.jsx
@@ -2,17 +2,17 @@ import PrimaryButton from "@/components/Buttons/PrimaryButton";
 import { Card, CardBody } from "@nextui-org/react";
 export default function BookingSection() {
   return (
-    <div className="container flex flex-col item-center justify-center mx-auto ">
-      <div className="grid md:grid-cols-2 md:mx-1" >
-        <Card className="lg:rounded-e-none bg-[#ff6600] p-8">
-          <CardBody className="text-center">
-            <p className="text-4xl text-white pb-3">Book on Your Time</p>
+    <div className="container flex flex-col item-center justify-center mx-auto pt-10 ">
+      <div className="grid md:grid-cols-2 md:mx-1 gap-10" >
+        <Card className="lg:rounded-e-none bg-[#ff6600] p-2">
+          <CardBody className="text-center flex justify-center">
+            <p className="md:text-4xl text-2xl text-white pb-3">Book on Your Time</p>
             <PrimaryButton textColor={'#ffff'} buttonTitle={'On time booking'} buttonLink={'/ntm'} />
           </CardBody>
         </Card>
-        <Card className="lg:rounded-s-none bg-[#1d284b] p-8">
-          <CardBody className="text-center">
-            <p className="text-4xl pb-3 text-white">Returning Customer Click here to log in to your hub</p>
+        <Card className="lg:rounded-s-none bg-[#1d284b] p-2">
+          <CardBody className="text-center flex justify-center">
+            <p className="md:text-4xl text-2xl pb-3 text-white">Returning Customer Click here to log in to your hub</p>
             <PrimaryButton textColor={'#ffff'} buttonTitle={'Login your hub'} buttonLink={'/ntm'} />
           </CardBody>
         </Card>
