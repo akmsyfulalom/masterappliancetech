affa30af0ef2b0533a925ddeeca1303e6aa59413
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-29 14:30:31 +0600

    remove svg

--- src/components/BookingFrom/InfoForBookingPage.jsx
+++ src/components/BookingFrom/InfoForBookingPage.jsx
@@ -13,28 +13,7 @@ export default function InfoForBookingPage() {
                         </div>
                         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                             <span className="relative inline-block">
-                                <svg
-                                    viewBox="0 0 52 24"
-                                    fill="currentColor"
-                                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                                >
-                                    <defs>
-                                        <pattern
-                                            id="b039bae0-fdd5-4311-b198-8557b064fce0"
-                                            x="0"
-                                            y="0"
-                                            width=".135"
-                                            height=".30"
-                                        >
-                                            <circle cx="1" cy="1" r=".7" />
-                                        </pattern>
-                                    </defs>
-                                    <rect
-                                        fill="url(#b039bae0-fdd5-4311-b198-8557b064fce0)"
-                                        width="52"
-                                        height="24"
-                                    />
-                                </svg>
+                               
                                 <span className="relative">Welcome</span>
                             </span>{' '}
                             to Master Appliance Tech
