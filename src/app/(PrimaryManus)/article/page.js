d20f40d5193da0608a182cba8740fd10e01ab7cf
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-29 16:35:20 +0600

    blacnk

--- src/app/(PrimaryManus)/article/page.js
+++ src/app/(PrimaryManus)/article/page.js
@@ -1,21 +1,32 @@
-import Blogs from "@/components/Article/Blogs";
-const BASE_URL = "https://masterappliancetech.vercel.app/"
+// import Blogs from "@/components/Article/Blogs";
+// const BASE_URL = "https://masterappliancetech.vercel.app/"
 
-const getData = async () => {
-  const res = await fetch(`${BASE_URL}json/masterapplienceNews.json`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data ok");
+// const getData = async () => {
+//   const res = await fetch(`${BASE_URL}json/masterapplienceNews.json`);
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data ok");
 
-  };
-  return res.json();
-};
+//   };
+//   return res.json();
+// };
 
-export default async function page() {
-  const blogs = await getData();
+// export default async function page() {
+//   const blogs = await getData();
 
+//   return (
+//     <div>
+//       <Blogs blogs={blogs} />
+//     </div>
+//   )
+// }
+
+import React from 'react'
+
+export default function page() {
   return (
     <div>
-      <Blogs blogs={blogs} />
+      man
     </div>
   )
 }
+
