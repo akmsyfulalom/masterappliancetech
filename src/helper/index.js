2853805fdbee61d27763b864f414ea4cf0467257
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-30 18:26:41 +0600

    resolve

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,37 +1,37 @@
-// const BASE_URL = "https://masterappliancetech.vercel.app/"
+const BASE_URL = "https://masterappliancetech.vercel.app/"
 
-// export const getData = async () => {
-//     const res = await fetch(`${BASE_URL}json/masterapplienceNews.json`);
-//     if (!res.ok) {
-//         throw new Error("Failed to fetch data");
+export const getData = async () => {
+    const res = await fetch(`${BASE_URL}json/masterapplienceNews.json`);
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
 
-//     };
-//     return res.json();
-// };
-
-
-// export const getBlogView = async (id) => {
-//     let item = await getData();
-
-//     if (!Array.isArray(item)) {
-//         item = item.items || [];
-//     }
-
-//     const singleItem = item.find(blog => blog?.id === id);
-//     console.log(singleItem);
-//     return singleItem;
-// };
+    };
+    return res.json();
+};
 
 
+export const getBlogView = async (id) => {
+    let item = await getData();
+
+    if (!Array.isArray(item)) {
+        item = item.items || [];
+    }
+
+    const singleItem = item.find(blog => blog?.id === id);
+    console.log(singleItem);
+    return singleItem;
+};
 
 
 
-// export const getReviews = async () => {
-//   const res = await fetch(`${BASE_URL}json/review.json`);
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data ok");
 
-//   };
-//   return res.json();
-// };
+
+export const getReviews = async () => {
+  const res = await fetch(`${BASE_URL}json/review.json`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data ok");
+
+  };
+  return res.json();
+};
 
