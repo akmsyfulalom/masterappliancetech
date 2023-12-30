bc79901d0b9cfe3f007420ec9426afc0f1066f60
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-30 18:34:42 +0600

    some

--- src/app/(PrimaryManus)/aboutUs/page.js
+++ src/app/(PrimaryManus)/aboutUs/page.js
@@ -18,7 +18,7 @@ export default function page() {
         />
         <div className="absolute top-0 left-0 w-full h-full bg-[#1d284b] opacity-85"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl md:text-3xl lg:text-5xl font-bold">ABOUT US</div>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-orange-500 w-[150px] h-[50px]  px-6">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-orange-500   px-6 pb-3">
           <p className="text-center text-white font-bold pt-4 flex items-center justify-center gap-2">
             <Link href="/">Home</Link>
             <FaLongArrowAltRight />
